576d435fa8de1d2011f56813bbd9c7ec08317b1e
aericjw <awalls26@rams.colostate.edu>
2021-11-11 13:24:17 -0700

    SQL Injection over HTTP GET

--- server/db.js
+++
@@ -1,7 +0,0 @@
-const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database("bank.sqlite", callback => { // bind database to variable
-    if (callback){
-        console.error(callback.message)
-    }
-});
-module.exports = db; // export database to be used by other JS files
